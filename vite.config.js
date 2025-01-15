f0f34981b9c58e0732bd35eba31f3a3710e08777
Ryan Yates <ryansyates@gmail.com>
2025-01-15 13:38:58 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/virtual-pro-tour/', // Replace with your repo name
+  build: {
+    outDir: 'docs' // Change build output to 'docs' instead of 'dist'
+  }
 })
